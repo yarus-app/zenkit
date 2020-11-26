f6d291cc177aa4776e9939471ddc57f56bd73b49
Yaroslav Usenko <usenko.jaroslav@gmail.com>
2020-11-26 18:44:52 +0200

    feat(box): added relative position

--- components/box/src/box.jsx
+++ components/box/src/box.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components/macro';
 
 export const Box = styled.div`
+  position: relative;
   display: ${({ inline }) => (inline ? 'inline-block' : 'block')};
   box-sizing: border-box;
 `;
